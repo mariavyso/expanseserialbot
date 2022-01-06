1064271a15a810d05510abc42f2f421655d25890
Высочина <vysocina@MacBook-Air-Vysocina.local>
2022-01-05 19:24:06 -0800

    df

--- expancebot.py
+++ expancebot.py
@@ -10,26 +10,22 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     bot.send_message(message.chat.id,"Привет ✌️ ")
 
-###@bot.message_handler(commands=['answer'])
-###def start_message(message):
-  
-  
-#   z = requests.get('https://soap4.me/soap/The_Expanse/6/')
-#   z.text
-#   import re
-#   regex = r"data:episode="
-#   test_str = z.text
-#   matches = re.finditer(regex, test_str, re.MULTILINE)
-#   for matchNum, match in enumerate(matches, start=1):
-#     print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-#   for groupNum in range(0, len(match.groups())):
-#         groupNum = groupNum + 1
-        
-#     print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+z = requests.get('https://soap4.me/soap/The_Expanse/6/')
+z.text
+import re
+regex = r"data:episode="
+test_str = z.text
+matches = re.finditer(regex, test_str, re.MULTILINE)
+for matchNum, match in enumerate(matches, start=1):
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
 
-# if matchNum > 3:
-#     print ('yes')
-# else:
-#     print ('no')
+
+@bot.message_handler(commands=['answer'])
+def start_message(message):
+    if matchNum > 3:
+        print ('Yes, there is a new episode for u!')
+        else:
+            print ('Oh,dear! You should wait')
  
 bot.infinity_polling()
