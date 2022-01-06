9e884d4b88f3fdc38c35667ec6e91c9dacf16a34
Высочина <vysocina@MacBook-Air-Vysocina.local>
2022-01-05 19:29:18 -0800

    jhh

--- expancebot.py
+++ expancebot.py
@@ -3,13 +3,6 @@ import json
 from html.parser import HTMLParser
 import telebot
 
-token = '5071473434:AAHwt3NGiWwiB_Z_5lEbYGDOa7gSoS7AYfo'
-bot = telebot.TeleBot(token)
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,"Привет ✌️ ")
-
 z = requests.get('https://soap4.me/soap/The_Expanse/6/')
 z.text
 import re
@@ -20,6 +13,12 @@ for matchNum, match in enumerate(matches, start=1):
     for groupNum in range(0, len(match.groups())):
         groupNum = groupNum + 1
 
+token = '5071473434:AAHwt3NGiWwiB_Z_5lEbYGDOa7gSoS7AYfo'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,"Привет ✌️ ")
 
 @bot.message_handler(commands=['answer'])
 def start_message(message):
