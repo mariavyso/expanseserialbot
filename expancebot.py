106962eaba80aa00a4d6655f9c47b884fadb7170
Высочина <vysocina@MacBook-Air-Vysocina.local>
2022-01-08 14:20:31 -0800

    ghhhh

--- expancebot.py
+++ expancebot.py
@@ -40,18 +40,7 @@ j = u.job_queue
         #bot.send_message(message.chat.id,'Oh,dear! You should wait')
 #job_minute = j.run_repeating(answer, interval=60, first=10)
 
-from telegram.ext import CommandHandler
-def callback_alarm(context: telegram.ext.CallbackContext):
-    context.bot.send_message(chat_id=context.job.context, text='BEEP')
 
-def callback_timer(update: telegram.Update, context: telegram.ext.CallbackContext):
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text='Setting a timer for 1 minute!')
-
-    context.job_queue.run_once(callback_alarm, 60, context=update.message.chat_id)
-
-timer_handler = CommandHandler('timer', callback_timer)
-u.dispatcher.add_handler(timer_handler)
 
 def callback_minute(context: telegram.ext.CallbackContext):
     if matchNum < a:
